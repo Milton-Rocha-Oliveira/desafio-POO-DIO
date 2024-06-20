256f3945db346784588b2aee42ee96f41f7e0e92
milton rocha <milton.j.r.oliveira.1990@gmail.com>
2024-06-19 21:11:31 -0300

    Criação projeto desadio DIO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -17,8 +16,8 @@ public class Main {
         curso2.setDescricao("Descrição curso JavaScript");
         curso2.setCargaHoraria(81);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+       /* System.out.println(curso1);
+        System.out.println(curso2);*/
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
@@ -26,5 +25,37 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+
+        bootCamp.setNome("Bootcamp Java Developer");
+        bootCamp.setDescricao(("Descrição: Bootcamp Java devoloper"));
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devMilton = new Dev();
+        devMilton.setNome("Milton");
+        devMilton.inscreverBootCamo(bootCamp);
+        devMilton.progredir();
+        System.out.println("Conteudos inscritos Milton " + devMilton.getConteudosInscritos());
+        System.out.println("******");
+        System.out.println("Conteudos concluidos Milton " + devMilton.getConteudosConcluidos());
+        System.out.println("XP : " + devMilton.calcularTotalXp());
+        System.out.println("*****");
+
+        Dev devEduardo = new Dev();
+        devEduardo.setNome("Eduardo");
+        devEduardo.inscreverBootCamo(bootCamp);
+        devEduardo.progredir();
+        devEduardo.progredir();
+        devEduardo.progredir();
+        System.out.println("Conteudos inscritos Eduardo " + devEduardo.getConteudosInscritos());
+        System.out.println("*****");
+        System.out.println("Conteudos concluidos Eduardo " + devMilton.getConteudosConcluidos());
+        System.out.println("XP : " + devMilton.calcularTotalXp());
+
+
+
     }
 }
